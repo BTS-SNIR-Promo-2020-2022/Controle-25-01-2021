ab19192410881882509919521320d8d98fc5b053
Julien Flusin <julienflusin3@gmail.com>
2021-01-25 14:22:40 +0100

    EXERCICE 3

--- Controle 25-01-2021/Controle 25-01-2021.c
+++ Controle 25-01-2021/Controle 25-01-2021.c
@@ -1,30 +1,67 @@
-#include <stdlib.h>
 #include <stdio.h>
-//EXERCICE 2
+#include <stdlib.h>
+int hauteur = 10, largeur = 20, x = 11, y = 6, increment1, increment2, increment3;
+char mouvement;
+void displaygrid() {
+    printf("\n");
+    for (increment1 = 1; increment1 <= largeur; increment1++) {
+        printf(" - ");
+    }
+    printf("\n");
+    for (increment2 = 1; increment2 < hauteur; increment2++) {
+        printf("|");
+        for (increment3 = 1; increment3 < largeur; increment3++) {
+            if (increment3 == x && increment2 == y) {
+                printf("*  ");
+            }
+            else {
+                printf("   ");
+            }
+        }
+        printf("|");
+        printf("\n");
+    }
+    for (increment1 = 1;increment1 <= largeur;increment1++) {
+        printf(" - ");
+    }
+}
 void main() {
-	int nombre1, nombre2, resultat;
-	char operation, continuer = 'y';
-	while (continuer == 'y') {
-		system("cls");
-		printf("Saisir deux nombres \n\n");
-		scanf("%d %d", &nombre1, &nombre2);
-		printf("Quel opération souhaitez-vous effectuer ?\n");
-		scanf(" %c", &operation);
-		switch (operation) {
-		case '+':
-			resultat = nombre1 + nombre2;
-			break;
-		case '-':
-			resultat = nombre1 - nombre2;
-			break;
-		case '*':
-			resultat = nombre1 * nombre2;
-			break;
-		case '/':
-			resultat = nombre1 / nombre2;
-			break;
-		}
-		printf("%d\nSouhaitez-vous continuer ? [y/n]", resultat);
-		scanf(" %c", &continuer);
-	}
+    displaygrid();
+    while (1) {
+        scanf(" %c", &mouvement);
+        switch (mouvement) {
+        case 'd':
+            system("cls");
+            x++;
+            displaygrid();
+            break;
+        case 'q':
+            system("cls");
+            x--;
+            displaygrid();
+            break;
+        case 'z':
+            system("cls");
+            y--;
+            displaygrid();
+            break;
+        case 's':
+            system("cls");
+            y++;
+            displaygrid();
+            break;
+        }
+        if (x > largeur) {
+            x = 2;
+        }
+        else if (x < 1) {
+            x = largeur;
+        }
+        if (y > hauteur) {
+            y = 2;
+        }
+        else if (y < 1) {
+            y = hauteur;
+        }
+    }
 }
