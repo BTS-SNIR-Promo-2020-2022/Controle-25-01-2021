752ab6db9d1ac0c87e55194e326de4fc7f88403b
Julien Flusin <julienflusin3@gmail.com>
2021-01-25 14:22:06 +0100

    EXERCICE 2

--- Controle 25-01-2021/Controle 25-01-2021.c
+++ Controle 25-01-2021/Controle 25-01-2021.c
@@ -1,8 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
-//EXERCICE 1
+//EXERCICE 2
 void main() {
-	for (int count = 1;count < 100 / 7;count++) {
-		printf("%d\n", count * 7);
+	int nombre1, nombre2, resultat;
+	char operation, continuer = 'y';
+	while (continuer == 'y') {
+		system("cls");
+		printf("Saisir deux nombres \n\n");
+		scanf("%d %d", &nombre1, &nombre2);
+		printf("Quel opération souhaitez-vous effectuer ?\n");
+		scanf(" %c", &operation);
+		switch (operation) {
+		case '+':
+			resultat = nombre1 + nombre2;
+			break;
+		case '-':
+			resultat = nombre1 - nombre2;
+			break;
+		case '*':
+			resultat = nombre1 * nombre2;
+			break;
+		case '/':
+			resultat = nombre1 / nombre2;
+			break;
+		}
+		printf("%d\nSouhaitez-vous continuer ? [y/n]", resultat);
+		scanf(" %c", &continuer);
 	}
 }
